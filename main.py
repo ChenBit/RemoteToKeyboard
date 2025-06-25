04c70d03193a4cd4004fdaf6ac963e2d28ae703b
Eric Chen <108616001+ChenBit@users.noreply.github.com>
2025-06-25 21:35:46 +0800

    Edited main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+import pyautogui
+
+# Initialization Pygame
+pygame.init()
+
+# Gain joystick data
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+if not joysticks:
+    exit()
+for i, j in enumerate(joysticks):
+    print(f"{i+1}. {j.get_name()}")
+
+# 用户选择设备
+selection = int(input("请选择编号: ")) - 1
+joystick = joysticks[selection]
+joystick.init()
+
+# 主循环
+pyautogui.FAILSAFE = False
+while True:
+    pygame.event.pump()
+    channel1 = joystick.get_axis(0) * 100
+    channel2 = joystick.get_axis(1) * 100
+    if 50 < channel1 <= 100:
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('d')
+    elif 2 < channel1 <= 50:
+        pyautogui.keyDown('d')
+    elif -50 < channel1 < -2:
+        pyautogui.keyDown('a')
+    elif -100 <= channel1 <= -50:
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('a')
+    pyautogui.keyUp('Shift')
+    pyautogui.keyUp('d')
+    pyautogui.keyUp('a')
+    
+    if 50 < channel2 <= 100:
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('w')
+    elif 2 < channel2 <= 50:
+        pyautogui.keyDown('w')
+    elif -50 < channel2 < -2:
+        pyautogui.keyDown('s')
+    elif -100 <= channel2 <= -50:
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('s')
+    pyautogui.keyUp('Shift')
+    pyautogui.keyUp('w')
+    pyautogui.keyUp('s')
